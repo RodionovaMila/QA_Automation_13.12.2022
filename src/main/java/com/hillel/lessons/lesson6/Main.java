ba19b653f0bfc94aa2551c5c6df16407143043d8
Liudmyla Rodionova <rodionovalyda64@gmail.com>
2023-01-06 13:57:47 +0200

    HW6

--- src/main/java/com/hillel/lessons/lesson6/Main.java
+++ src/main/java/com/hillel/lessons/lesson6/Main.java
@@ -17,7 +17,7 @@ public class Main {
         String Team2 = scanner.nextLine();
         System.out.println("Team2 = " + Team2);
 
-        scanner.nextLine();
+//        scanner.nextLine();
 
 
         int team1Player1 = 0;
@@ -33,28 +33,43 @@ public class Main {
 
         if (scanner.hasNextInt()) {
             team1Player1 = scanner.nextInt();
+            System.out.println("team1Player1 = " + team1Player1);
+        }
+        if (scanner.hasNextInt()) {
             team1Player2 = scanner.nextInt();
+            System.out.println("team1Player2 = " + team1Player2);
+        }
+        if (scanner.hasNextInt()) {
             team1Player3 = scanner.nextInt();
+            System.out.println("team1Player3 = " + team1Player3);
+        }
+        if (scanner.hasNextInt()) {
             team1Player4 = scanner.nextInt();
+            System.out.println("team1Player4 = " + team1Player4);
+        }
+        if (scanner.hasNextInt()) {
             team1Player5 = scanner.nextInt();
+            System.out.println("team1Player5 = " + team1Player5);
+        }
+        if (scanner.hasNextInt()) {
             team2Player1 = scanner.nextInt();
+            System.out.println("team2Player1 = " + team2Player1);
+        }
+        if (scanner.hasNextInt()) {
             team2Player2 = scanner.nextInt();
+            System.out.println("team2Player2 = " + team2Player2);
+        }
+        if (scanner.hasNextInt()) {
             team2Player3 = scanner.nextInt();
+            System.out.println("team2Player3 = " + team2Player3);
+        }
+        if (scanner.hasNextInt()) {
             team2Player4 = scanner.nextInt();
+            System.out.println("team2Player4 = " + team2Player4);
+        }
+        if (scanner.hasNextInt()) {
             team2Player5 = scanner.nextInt();
-
-            System.out.println("team1Player1 = " + team1Player1 );
-            System.out.println("team1Player2 = " + team1Player2 );
-            System.out.println("team1Player3 = " + team1Player3 );
-            System.out.println("team1Player4 = " + team1Player4 );
-            System.out.println("team1Player5 = " + team1Player5 );
-            System.out.println("team2Player1 = " + team2Player1 );
-            System.out.println("team2Player2 = " + team2Player2 );
-            System.out.println("team2Player3 = " + team2Player3 );
-            System.out.println("team2Player4 = " + team2Player4 );
-            System.out.println("team2Player5 = " + team2Player5 );
-
-
+            System.out.println("team2Player5 = " + team2Player5);
 
         } else {
             System.out.println("Wrong data!");
@@ -71,12 +86,8 @@ public class Main {
         } else if (resultTeam2 == resultTeam1) {
             System.out.println("Нічия");
         } else {
-            System.out.println("Перемогла команда "  + name1 + " набрала " + resultTeam1 + " очків");
-          }
+            System.out.println("Перемогла команда " + name1 + " набрала " + resultTeam1 + " очків");
+        }
 
     }
-    }
-
-
-
-
+}
