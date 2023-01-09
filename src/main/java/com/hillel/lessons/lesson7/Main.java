f20a7614d0cf0757f46cb0f3a4c6d9b672e48bb4
Liudmyla Rodionova <rodionovalyda64@gmail.com>
2023-01-09 19:27:28 +0200

    HW8

--- src/main/java/com/hillel/lessons/lesson7/Main.java
+++ src/main/java/com/hillel/lessons/lesson7/Main.java
@@ -1,38 +1,32 @@
 package com.hillel.lessons.lesson7;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    private static boolean LuckyNumbers(int num) {
 
-        int i = num % 10;
-        do {
-            if(i == 4 || i == 9)
-                return false;
-            num /= 10;
-            i = num % 10;
-        }while (num > 0);
-        return true;
-
-
-    }
+    private static final String guessNumber = "Угадай число: ";
+    private static final String Congratulation = "Поздравляем!!! Вы выиграли!";
+    private static final int attemptsNumber = 3;
 
     public static void main(String[] args) {
-        int counter = 0;
-        int num = 1;
-        int num1 = 1;
-        while (num <= 100) {
-            if (LuckyNumbers(num1)) {
-                System.out.println(num1);
-                counter++;
+        Scanner in = new Scanner(System.in);
+        Random random = new Random(System.currentTimeMillis());
+        final int randomNumber = random.nextInt(10);
+        int usersNumber;
+        for (int i = 0; i < attemptsNumber; i++) {
+            System.out.println(guessNumber);
+            usersNumber = in.nextInt();
+            if (randomNumber == usersNumber) {
+                System.out.println(Congratulation);
+                break;
 
-                num++;
-
-            }
-            num1++;
-
+            } else
+                System.out.println(usersNumber > randomNumber ? "Моё число меньше" : "Моё число больше");
         }
-
-        System.out.println("counter = " + counter);
-
+        System.out.printf("Загаданное число %d%n", randomNumber);
     }
-
 }
+
+
+
