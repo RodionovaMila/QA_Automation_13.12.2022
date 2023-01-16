5ae014a248d5370b4b35c9f76ca9e42efe818d8f
Liudmyla Rodionova <rodionovalyda64@gmail.com>
2023-01-16 22:02:46 +0200

    HW10

---
+++ src/main/java/com/hillel/lessons/lesson10/Main.java
@@ -0,0 +1,41 @@
+package com.hillel.lessons.lesson10;
+
+    import java.util.Arrays;
+    import java.util.Random;
+    import java.util.Scanner;
+
+    public class Main {
+        public static void main(String[] args) {
+
+         Scanner str = new Scanner(System.in);
+         int x = str.nextInt();
+         int a[][] = new int[x][];
+         for (int n = 0; n < a.length; n++) {
+             a[n] = new int[x];
+             for (int m = 0; m < a[n].length; m++) {
+                 a[n][m] = (int) (Math.random() * 100);
+                 System.out.print(a[n][m] + "\t");
+             }
+             System.out.println();
+         }
+                // часть 2 - транспонирование матрицы
+         for (int n = 0; n < a.length; n++) {
+              for (int m = n + 1; m < a.length; m++) {
+              int temp = a[n][m];
+              a[n][m] = a[m][n];
+              a[m][n] = temp;
+                    }
+                }
+
+                // часть 3 - выводит на экран транспонированную матрицу
+              System.out.println();
+              System.out.println("Новая матрица");
+              System.out.println("------");
+                for (int n = 0; n < a.length; n++) {
+                    for (int m = 0; m < a[n].length; m++) {
+                        System.out.print(a[n][m] + "\t");
+                    }
+              System.out.println();
+                }
+            }
+        }
