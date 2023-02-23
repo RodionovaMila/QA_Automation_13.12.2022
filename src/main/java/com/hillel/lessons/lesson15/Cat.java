e7dc59f6cb10678928b1eb3fb652ac06fe2de43a
Liudmyla Rodionova <rodionovalyda64@gmail.com>
2023-02-23 19:26:50 +0200

    HW15

--- src/main/java/com/hillel/lessons/lesson15/Cat.java
+++ src/main/java/com/hillel/lessons/lesson15/Cat.java
@@ -1,18 +1,14 @@
 package com.hillel.lessons.lesson15;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
-public class Cat {
-
+    public class Cat {
     private String name;
     private int age;
     private int weight;
 
     public Cat(String name, int age, int weight) {
-        this.name = name;
-        this.age = age;
-        this.weight = weight;
+        setName(name);
+        setAge(age);
+        setWeight(weight);
     }
 
     public Cat() {
@@ -27,70 +23,58 @@ public class Cat {
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Имя кота не может быть пустым!");
+        }
+        if (!name.matches("^[a-zA-Z]+$")) {
+            throw new IllegalArgumentException("Имя кота может содержать только буквы!");
+        }
+        this.name = name.trim();
     }
-
 
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
-        if (age >= 0) {
-            this.age = age;
-        } else {
-            System.out.println("Ошибка! Возраст не может быть отрицательным числом!");
+        if (age < 0 || age > 20) {
+            throw new IllegalArgumentException("Возраст кота должен быть в диапазоне от 0 до 20 лет!");
         }
+        this.age = age;
     }
-
 
     public int getWeight() {
         return weight;
     }
 
     public void setWeight(int weight) {
+        if (weight < 0 || weight > 30) {
+            throw new IllegalArgumentException("Вес кота должен быть в диапазоне от 0 до 30 кг!");
+        }
         this.weight = weight;
     }
 
-    public static boolean isAlpha(String s)
-    {
-        if (s == null) {
-            return false;
-        }
-
-        for (int i = 0; i < s.length(); i++)
-        {
-            char c = s.charAt(i);
-            if (!(c >= 'А' && c <= 'Я') && !(c >= 'а' && c <= 'я')) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-
-
     public static void main(String[] args) {
-
-        Cat murchik = new Cat("Мурчик", 5, 4);
-
-        String murchikName = murchik.getName();
-
-        System.out.println("Изначальное имя кота — " + murchik.getName());
-        murchik.setName("Рыжик");
-        System.out.println("Новое имя кота — " + murchik.getName());
-
-        int murchikAge = murchik.getAge();
-        murchik.setAge(-1000);
-
-        System.out.println("Возраст Рыжика — " + murchik.getAge() + " лет");
-
-        int murchikWeight = murchik.getWeight();
-        System.out.println("Вес кота: " + murchikWeight);
-
-        String s = "АБВГ";
-        System.out.println("IsAlpha: " + isAlpha(s));
-
+        Cat cat = new Cat();
+        try {
+            cat.setName("");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // выводит "Имя кота не может быть пустым!"
+        }
+        try {
+            cat.setName("Мурзик123");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // выводит "Имя кота может содержать только буквы!"
+        }
+        try {
+            cat.setAge(-1000);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // выводит "Возраст кота должен быть в диапазоне от 0 до 20 лет!"
+        }
+        try {
+            cat.setWeight(50);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // выводит "Вес кота должен быть в диапазоне от 0 до 30 кг!"
+        }
     }
 }
